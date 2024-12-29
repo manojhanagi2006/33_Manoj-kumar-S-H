dc3dbeb1489a903fc5be144cfd56f8e52770936b
manojhanagi2006 <manojhanagi2006@gmail.com>
2024-12-29 23:12:46 +0530

    abc

---
+++ newfile1.py
@@ -0,0 +1,12 @@
+# Using a while loop to print numbers from 1 to 5
+print("Numbers from 1 to 5 using while loop:")
+number = 1
+while number <= 5:
+    print(number)
+    number += 1
+
+# Using a for loop to print numbers from 6 to 10
+print("Numbers from 6 to 10 using for loop:")
+for number in range(6, 11):
+    print(number)
+[A[B
